e087f0c62f61e86e0c924130a7a5be8f1f042632
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-03 09:26:36 +0100

    Rocket setup.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::async_stream::stream;
+use rocket::futures::{SinkExt, StreamExt};
+use rocket::routes;
+use rocket_ws::{Channel, WebSocket};
+
+#[rocket::get("/")]
+fn chat(ws: WebSocket) -> Channel<'static> {
+    ws.channel(move |mut stream| {
+        Box::pin(async move {
+            while let Some(message) = stream.next().await {
+                let _ = stream.send(message?).await;
+            }
+            Ok(())
+        })
+    })
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build().mount("/", routes![chat]).launch().await;
 }
