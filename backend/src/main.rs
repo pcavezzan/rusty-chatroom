efbdca3600cf0b0bd89fd045bf3fba92b4bab3a2
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-08 15:37:47 +0100

    Chatroom refactoring.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
+use rocket::async_stream::stream;
+use rocket::futures::stream::SplitSink;
+use rocket::futures::{SinkExt, StreamExt};
+use rocket::tokio::sync::Mutex;
+use rocket::{routes, State};
+use rocket_ws::stream::DuplexStream;
+use rocket_ws::{Channel, Message, WebSocket};
 use std::collections::HashMap;
 use std::os::macos::raw::stat;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use rocket::async_stream::stream;
-use rocket::futures::{SinkExt, StreamExt};
-use rocket::futures::stream::SplitSink;
-use rocket::{routes, State};
-use rocket::tokio::sync::Mutex;
-use rocket_ws::{Channel, Message, WebSocket};
-use rocket_ws::stream::DuplexStream;
 
 static USER_ID_COUNTER: AtomicUsize = AtomicUsize::new(0);
 
@@ -16,30 +16,36 @@ struct ChatRoom {
     connections: Mutex<HashMap<usize, SplitSink<DuplexStream, Message>>>,
 }
 
+impl ChatRoom {
+    pub async fn add(&self, id: usize, sink: SplitSink<DuplexStream, Message>) {
+        let mut cons = self.connections.lock().await;
+        cons.insert(id, sink);
+    }
+
+    pub async fn remove(&self, id: usize) {
+        let mut cons = self.connections.lock().await;
+        cons.remove(&id);
+    }
+
+    pub async fn broadcast(&self, message: Message) {
+        let mut cons = self.connections.lock().await;
+        for (_, sink) in cons.iter_mut() {
+            let _ = sink.send(message.clone()).await;
+        }
+    }
+}
+
 #[rocket::get("/")]
 fn chat<'r>(ws: WebSocket, state: &'r State<ChatRoom>) -> Channel<'r> {
     ws.channel(move |mut stream| {
         Box::pin(async move {
             let user_id = USER_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
             let (ws_sink, mut ws_stream) = stream.split();
-            {
-                let mut cons = state.connections.lock().await;
-                cons.insert(user_id, ws_sink);
-            }
+            state.add(user_id, ws_sink).await;
             while let Some(message) = ws_stream.next().await {
-                {
-                    let msg = message?;
-                    let mut cons = state.connections.lock().await;
-                    for (_, sink) in cons.iter_mut() {
-                        let _ = sink.send(msg.clone()).await;
-                    }
-                }
+                state.broadcast(message?).await;
             }
-
-            {
-                let mut cons = state.connections.lock().await;
-                cons.remove(&user_id);
-            }
+            state.remove(user_id).await;
             Ok(())
         })
     })
@@ -50,5 +56,6 @@ async fn main() {
     let _ = rocket::build()
         .mount("/", routes![chat])
         .manage(ChatRoom::default())
-        .launch().await;
+        .launch()
+        .await;
 }
