78a27480941d9aa84440821fc128a4985c50e569
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-12-08 16:50:54 +0100

    Hooks and websocket connection.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,9 +1,26 @@
 use yew::prelude::*;
+use yew_hooks::use_websocket;
 
 #[function_component]
 fn App() -> Html {
+    let messages_handle = use_state(Vec::new);
+    let messages = (*messages_handle).clone();
+
+    let ws = use_websocket("ws://127.0.0.1:8000".to_string());
+
+    let mut cloned_messages = messages.clone();
+    use_effect_with(ws.message, move |ws_message| {
+        if let Some(ws_msg) = &**ws_message {
+            cloned_messages.push(ws_msg.clone());
+            messages_handle.set(cloned_messages);
+        }
+    });
     html! {
-        <div id="chat"></div>
+        <ul id="chat">
+        {
+            messages.iter().map(|msg| html! {<li>{msg}</li>}).collect::<Html>()
+        }
+        </ul>
     }
 }
 
